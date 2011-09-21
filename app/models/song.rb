b3bd28c7b039152f9dc03dd2df0c683b74ab59c0
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-09-22 09:51:05 +1000

    Added Song transfer file logic

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,4 +6,27 @@ class Song < ActiveRecord::Base
     "#{artist} [#{album}] - #{name}"
   end
 
+  def download
+    transfer_file
+  end
+
+  def transfer_file(&block)
+    url = URI.parse(song_url)
+    resp = Net::HTTP.start(url.host, url.port) { |http| http.get(url.path) }
+    file = File.new(Rails.root.join('tmp', id.to_s), "w+")
+    file.write resp.body.force_encoding("UTF-8")
+    yield file if block_given?
+    file.close
+  end
+
+  def song_url
+    locate_best_track.url
+  end
+
+  def locate_best_track
+    tracks.sort { |a,b| b <=> a }.find { |track|
+      track.library.online?
+    }
+  end
+
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -2,5 +2,9 @@ class Track < ActiveRecord::Base
   belongs_to :song
   belongs_to :library
 
-  
+  def url
+    uri = URI.parse(library.url)
+    uri.path = "/tracks/#{guid}/download"
+    uri.to_s
+  end
 end
