51fd1bbbba9553668ce9fe7d98913b9817330667
Ivan Vanderbyl <ivanvanderbyl@me.com>
2011-09-21 19:29:05 +1000

    Added browser

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,4 +1,28 @@
 class Library < ActiveRecord::Base
   has_many :tracks
-  has_many :songs, :throught => :tracks
+  has_many :songs, :through => :tracks
+
+  def self.create_or_update_from_dnssd_service(service)
+    library = find_or_create_by_name(service.name)
+
+    if service.flags.add?
+      DNSSD::Service.new.resolve service do |r|
+        # puts "#{r.name} on #{r.target}:#{r.port}"
+        # puts "\t#{r.text_record.inspect}" unless r.text_record.empty?
+
+        library.update_attribute(:url, url_for_hostname(r.target, r.port))
+        break unless r.flags.more_coming?
+      end
+
+      library.update_attribute(:online, true)
+    else
+      library.update_attribute(:online, false)
+    end
+
+    p library
+  end
+
+  def self.url_for_hostname(hostname, port)
+    "http://#{hostname}:#{port}"
+  end
 end
--- lib/dj/browser.rb
+++ lib/dj/browser.rb
@@ -1,4 +1,40 @@
+require 'dnssd'
+require File.expand_path('../../../config/environment', __FILE__)
+
 module DJ
   class Browser
+
+    def initialize
+      browse
+    end
+
+    def browse
+      browser = DNSSD::Service.new
+      services = {}
+
+      puts "Browsing for DJJour services\r\n"
+
+      browser.browse '_djjour._tcp' do |reply|
+        services[reply.fullname] = reply
+        next if reply.flags.more_coming?
+
+        services.sort_by { |_, service|
+          [(service.flags.add? ? 0 : 1), service.fullname]
+        }.each do |_, service|
+
+          Library.create_or_update_from_dnssd_service(service)
+
+          # add = service.flags.add? ? 'Add' : 'Remove'
+          # puts "#{add} #{service.name} on #{service.domain}"
+
+          # break unless service.flags.more_coming?
+        end
+
+        services.clear
+
+        puts
+      end
+    end
+
   end
 end
